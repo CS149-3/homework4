e65239285ef0fbe82006918165b5059d5efb0598
isabelchan <chanisabelbel@yahoo.com>
2016-03-04 16:55:32 -0800

    Update MFU.java

--- src/Paging/MFU.java
+++ src/Paging/MFU.java
@@ -4,10 +4,52 @@ import java.util.ArrayList;
 
 public class MFU implements Algorithm {
 
-	@Override
 	public Integer pageReferenced(int page, ArrayList<Page> disk, ArrayList<Page> memory) {
-		// TODO Auto-generated method stub
+		if(memory.size()<1){
+			for(int i=0;i<disk.size();i++){
+				if(disk.get(i).getPageId()==page){
+					memory.add(disk.get(i));
+					disk.remove(i);
+					return page;
+				}
+			}
+		}
+		for(int i=0;i<memory.size();i++){
+			if(memory.get(i).getPageId()==page){
+				return null;
+			}
+		}
+		if(memory.size()<4){
+			for(int i=0;i<disk.size();i++){
+				if(disk.get(i).getPageId()==page){
+					memory.add(disk.get(i));
+					disk.remove(i);
+					return page;
+				}
+			}
+
+		}
+		else{
+			Page mostFrequentlyUsedPage=memory.get(0);
+			int indexOfMemory=0;
+			Page temp=null;
+			for(int i=1;i<memory.size();i++){
+				if(Page.compareByMostFrequent().compare(mostFrequentlyUsedPage, memory.get(i))==1){
+					mostFrequentlyUsedPage=memory.get(i);
+					indexOfMemory=i;
+				}
+			}
+			for(int i=0;i<disk.size();i++){
+				if(disk.get(i).getPageId()==page){
+					temp=disk.get(i);
+					disk.remove(i);
+				}
+			}
+			memory.remove(indexOfMemory);
+			disk.add(mostFrequentlyUsedPage);
+			memory.add(temp);
+			return mostFrequentlyUsedPage.getPageId();
+		}
 		return null;
 	}
-
 }
