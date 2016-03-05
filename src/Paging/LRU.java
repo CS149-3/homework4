76192868e9a35bfa881e189392a55ce6f03f45f5
Sean Vail <seanv554@gmail.com>
2016-03-04 21:07:39 -0800

    FIFO and LRU Implemented

--- src/Paging/LRU.java
+++ src/Paging/LRU.java
@@ -1,13 +1,37 @@
 package Paging;
 
 import java.util.ArrayList;
+import java.util.function.Predicate;
 
 public class LRU implements Algorithm {
 
 	@Override
 	public Integer pageReferenced(int page, ArrayList<Page> disk, ArrayList<Page> memory) {
-		// TODO Auto-generated method stub
-		return null;
+		Page newPage;
+		if(memory.contains(newPage = disk.get(page))){
+			return null;
+		}
+		
+		if(memory.size() < 4){
+			memory.add(newPage);
+			return null;
+		}
+		
+		
+		ArrayList<Page> memoryCopy = memory;
+		memoryCopy.sort(Page.compareByLastUsed());
+		int evictPage = memoryCopy.get(0).getPageId();
+		
+		memory.removeIf(new Predicate<Page>() {
+			@Override
+			public boolean test(Page p1) {
+				return p1.getPageId() == evictPage;
+			}
+			
+		});
+		
+		memory.add(newPage);
+		return evictPage;
 	}
 
 }
